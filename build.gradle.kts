8f93acaa1c36af42bb9bbd44dd90e7082728661d
Suvesh Moza <hellosuvesh@gmail.com>
2026-06-03 20:25:53 +0530

    chore: upgrade AGP to 9.2.1
    Signed-off-by: Suvesh Moza <hellosuvesh@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("com.android.application") version "9.0.0" apply false
-    kotlin("android") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.0" apply false
 }
