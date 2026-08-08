553534a06f0149b57fef41b82475f407ec21511f
Suvesh Moza <hellosuvesh@gmail.com>
2026-08-08 19:40:09 +0530

    chore: bump AGP to 9.3.1 and Kotlin to 2.4.10
    Signed-off-by: Suvesh Moza <hellosuvesh@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10"
 }
 
 android {
